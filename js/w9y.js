2376a1ba4ce658912626a22f921a049d299ce927
Broken Pipe <navigaid@gmail.com>
2026-07-11 23:31:57 +0800

    bump

--- js/w9y.js
+++ js/w9y.js
@@ -1,5 +1,5 @@
 export const CDN = {
-  sh: 'https://w9y.up.railway.app/go/github.com/btwiuse/hush/cmd/hush@main',
-  kernel: 'https://w9y.up.railway.app/go/github.com/justwasm/hackpad/cmd/kernel@main',
-  editor: 'https://w9y.up.railway.app/go/github.com/justwasm/hackpad/cmd/editor@main',
+  sh: 'https://w9y.up.railway.app/go/github.com/btwiuse/hush/cmd/hush@v0.5.4',
+  kernel: 'https://w9y.up.railway.app/go/github.com/justwasm/hackpad/cmd/kernel@ce74d59ae10d3a98ab66c04056ac0533c10904e8',
+  editor: 'https://w9y.up.railway.app/go/github.com/justwasm/hackpad/cmd/editor@ce74d59ae10d3a98ab66c04056ac0533c10904e8',
 }
